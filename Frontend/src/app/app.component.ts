a354e0383243da132ac7dcc58d872923aad53fff
Matías Hermosilla <matiasahermosilla@gmail.com>
2019-04-23 16:27:50 -0400

    Agregando gráfico de porcentaje

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Frontend';
+
+    public porcentajePapel = 10;
+
+    public get outerStrokeColor(): string {
+        if ( this.porcentajePapel <= 10 ) {
+            return '#EF9A9A';
+        } else if ( this.porcentajePapel <= 30 ) {
+            return '#FFAB91';
+        } else if ( this.porcentajePapel <= 50 ) {
+            return '#FFF59D';
+        } else if ( this.porcentajePapel <= 85 ) {
+            return '#A5D6A7';
+        }
+        return '#90CAF9';
+    }
+
+    public get innerStrokeColor(): string {
+        if ( this.porcentajePapel <= 10 ) {
+            return '#EF5350';
+        } else if ( this.porcentajePapel <= 30 ) {
+            return '#FF7043';
+        } else if ( this.porcentajePapel <= 50 ) {
+            return '#FFEE58';
+        } else if ( this.porcentajePapel <= 85 ) {
+            return '#66BB6A';
+        }
+        return '#42A5F5';
+    }
+
 }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -11,8 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgCircleProgressModule.forRoot(),
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
